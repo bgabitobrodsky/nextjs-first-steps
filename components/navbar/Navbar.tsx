72190d6dfb33defdebcff0e342f11bf48ae011da
bgabitobrodsky <bernardogabito@gmail.com>
2023-12-29 15:07:33 -0300

    first client-side component

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,28 +1,25 @@
 import { HomeIcon } from "@primer/octicons-react";
 import Link from "next/link";
+import { ActiveLink } from "..";
 
 const navItems = [
-    {path:'/about',text:'About'},
-    {path:'/pricing',text:'Pricing'},
-    {path:'/contact',text:'Contact'},
-]
+	{ path: "/about", text: "About" },
+	{ path: "/pricing", text: "Pricing" },
+	{ path: "/contact", text: "Contact" },
+];
 
-export const Navbar = async() => {
+export const Navbar = async () => {
 	return (
 		<nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
-			<Link className="flex items-center" href={'/'} >
-                <HomeIcon className="mr-2"/>
+			<Link className="flex items-center" href={"/"}>
+				<HomeIcon className="mr-2" />
 				Home
 			</Link>
 			<div className="flex flex-1"></div>
 
-            {
-                navItems.map( navItem => (
-                    <Link key={navItem.text} className="mr-2" href={navItem.path}>
-                        {navItem.text}
-                    </Link>
-                ))
-            }
+			{navItems.map((navItem) => (
+				<ActiveLink key={navItem.text} {...navItem} />
+			))}
 		</nav>
 	);
 };
