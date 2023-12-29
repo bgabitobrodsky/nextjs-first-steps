9eb827e89b7a9fba1364776f8dc04dc3c7f01e91
bgabitobrodsky <bernardogabito@gmail.com>
2023-12-29 12:25:04 -0300

    metadata for single page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,15 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "About",
+	description: "SEO Description",
+	keywords: ["About Page", "Información"],
+};
+
 export default function AboutPage() {
-    return (
-        <>
-            <span className="text-7xl">About Page</span>
-        </>
-    )
-} 
+	return (
+		<>
+			<span className="text-7xl">About Page</span>
+		</>
+	);
+}
